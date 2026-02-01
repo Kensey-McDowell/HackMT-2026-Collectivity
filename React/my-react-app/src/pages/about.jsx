6760b912ac0f3efb86c26cdd265dc308069d933d
Vincent Truong <vincenttruong329@gmail.com>
2026-02-01 12:39:19 +0000

    Added Profile Cards to About Us Carousel

--- React/my-react-app/src/pages/about.jsx
+++ React/my-react-app/src/pages/about.jsx
@@ -1,149 +1,41 @@
-import React from 'react';
-import './about.css';
-import GavinPic from '../assets/images/Gavin.png';
-import KenseyPic from '../assets/images/Kensey.png';
-import ErikPic from '../assets/images/Erik.png';
-import RileyPic from '../assets/images/Erik.png';
-import DevinPic from '../assets/images/Devin.png';
-import VincentPic from '../assets/images/Vincent.png';
-import VivianPic from '../assets/images/Vivian.png';
-import CamPic from '../assets/images/Cam.png';
-import AlexPic from '../assets/images/Alex.png';
-import DrewPic from '../assets/images/Drew.png';
-import CatPic from '../assets/images/goofyahcat.png';
+// src/pages/AboutPage.jsx
+import React from "react";
+import "./about.css";
+import { teamMembers } from "./data/teamMembers.js";
+import TeamMemberCard from "../components/TeamMemberCard";
+
 export default function AboutPage() {
   return (
-    
     <div className="about-container">
-      {/* Jump links */}
-      <div className="about-jump-links">
-        <a href="#mission">Mission</a>
-        <a href="#technology">Technology</a>
-        <a href="#why-us">Why Us</a>
-      </div>
 
       {/* Sliding text */}
       <div className="about-slide-up-text">About Collectivity</div>
 
-      
-        {/* From Uiverse.io by PriyanshuGupta28 */}
-        <div className="marquee">
-          <div className="marquee_header">See the creators!</div>
-          <div className="marquee__inner">
-            <div className="marquee__group">
-              <span className="scroll-image-container"> 
-                <img src={GavinPic} alt = "GavinPic"/> 
-                <span className="picture"> Gavin Liles</span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={KenseyPic} alt = "KenseyPic"/> 
-                <span className="picture"> Kensey McDowell </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={ErikPic} alt = "ErikPic"/> 
-                <span className="picture"> Erik Apruda </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={RileyPic} alt = "RileyPic"/> 
-                <span className="picture"> Riley Wells </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={DevinPic} alt = "DevinPic"/> 
-                <span className="picture"> Devin Latham </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={VincentPic} alt = "VincentPic"/> 
-                <span className="picture"> Vincent Truong </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={VivianPic} alt = "VivianPic"/> 
-                <span className="picture"> Vivian O'Connor </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CamPic} alt = "CamPic"/> 
-                <span className="picture"> Camille Murnane </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={AlexPic} alt = "AlexPic"/> 
-                <span className="picture"> Alex Guzman </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={DrewPic} alt = "DrewPic"/> 
-                <span className="picture"> Drew Hammer </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "AlecPic"/> 
-                <span className="picture"> Alec Creasy </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "TristenPic"/> 
-                <span className="picture"> Tristen Onate </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "JackPic"/> 
-                <span className="picture"> Jack Quinn </span>
-                </span>
-            </div>
+      {/* Team Carousel / Marquee */}
+      <div className="marquee">
+        <div className="marquee_header">See the creators!</div>
+        <div className="marquee__inner">
+          {/* First group for continuous scroll */}
+          <div className="marquee__group">
+            {teamMembers.map((member, idx) => (
+              <div key={`team-a-${idx}`} className="scroll-image-container">
+                <TeamMemberCard member={member} />
+              </div>
+            ))}
+          </div>
 
-            <div className="marquee__group">
-              <span className="scroll-image-container"> 
-                <img src={GavinPic} alt = "GavinPic"/> 
-                <span className="picture"> Gavin Liles </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={KenseyPic} alt = "KenseyPic"/> 
-                <span className="picture"> Kensey McDowell </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={ErikPic} alt = "ErikPic"/> 
-                <span className="picture"> Erik Apruda </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={RileyPic} alt = "RileyPic"/> 
-                <span className="picture"> Riley Wells</span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={DevinPic} alt = "DogPic"/> 
-                <span className="picture"> Devin Latham </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={VincentPic} alt = "DogPic"/> 
-                <span className="picture"> Vincent Truong </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={VivianPic} alt = "DogPic"/> 
-                <span className="picture"> Vivian O'Connor </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CamPic} alt = "DogPic"/> 
-                <span className="picture"> Camille Murnane </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={AlexPic} alt = "DogPic"/> 
-                <span className="picture"> Alex Guzman </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={DrewPic} alt = "DrewPic"/> 
-                <span className="picture"> Drew Hammer </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "AlecPic"/> 
-                <span className="picture"> Alec Creasy </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "TristenPic"/> 
-                <span className="picture"> Tristen Onate </span>
-                </span>
-              <span className="scroll-image-container"> 
-                <img src={CatPic} alt = "JackPic"/> 
-                <span className="picture"> Jack Quinn</span>
-                </span>
-            </div>
+          {/* Duplicate group for infinite scroll */}
+          <div className="marquee__group">
+            {teamMembers.map((member, idx) => (
+              <div key={`team-b-${idx}`} className="scroll-image-container">
+                <TeamMemberCard member={member} />
+              </div>
+            ))}
           </div>
         </div>
+      </div>
 
-      
-
+      {/* Content Sections */}
       <div className="about-card" id="mission">
         <h2>Our Mission</h2>
         <p>
@@ -155,15 +47,17 @@ export default function AboutPage() {
       <div className="about-card" id="technology">
         <h2>Our Technology</h2>
         <p>
-          Our platform connects to the blockchain to verify authenticity of each collectible.
-          The smart contract ensures all transactions are secure and tamper-proof.
+          Our platform connects to the blockchain to verify authenticity of each
+          collectible. The smart contract ensures all transactions are secure and
+          tamper-proof.
         </p>
       </div>
 
       <div className="about-card" id="why-us">
         <h2>Why Choose Us?</h2>
         <p>
-          From rare Pokémon cards to limited-edition collectibles, we provide a trustworthy marketplace.
+          From rare Pokémon cards to limited-edition collectibles, we provide a
+          trustworthy marketplace.
         </p>
       </div>
 
---
+++ React/my-react-app/src/pages/data/teamMembers.js
@@ -0,0 +1,104 @@
+import GavinPic from "../../assets/images/Gavin.png";
+import KenseyPic from "../../assets/images/Kensey.png";
+import ErikPic from "../../assets/images/Erik.png";
+import RileyPic from "../../assets/images/goofyahcat.png";
+import DevinPic from "../../assets/images/Devin.png";
+import VincentPic from "../../assets/images/Vincent.png";
+import VivianPic from "../../assets/images/Vivian.png";
+import CamPic from "../../assets/images/Cam.png";
+import AlexPic from "../../assets/images/Alex.png";
+import DrewPic from "../../assets/images/Drew.png";
+import CatPic from "../../assets/images/goofyahcat.png";
+export const teamMembers = [
+  {
+    name: "Gavin Liles",
+    role: "Computer Science",
+    image: GavinPic,
+    linkedin: "https://www.linkedin.com/in/gavin-liles-2ab327264/",
+    email: "#",
+  },
+  {
+    name: "Kensey McDowell",
+    role: "Computer Science",
+    image: KenseyPic,
+    linkedin: "https://www.linkedin.com/in/kensey-mcdowell-5024b4264/",
+    email: "#",
+  },
+  {
+    name: "Erik Apruda",
+    role: "Computer Science",
+    image: ErikPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Riley Wells",
+    role: "Computer Science",
+    image: RileyPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Devin Latham",
+    role: "Computer Science",
+    image: DevinPic,
+    linkedin: "https://www.linkedin.com/in/devin-latham/",
+    email: "devinlatham20@gmail.com",
+  },
+  {
+    name: "Vincent Truong",
+    role: "Cybersecurity",
+    image: VincentPic,
+    linkedin: "https://www.linkedin.com/in/vincent-truong-3481a8291/",
+    email: "vincenttruong329@gmail.com",
+  },
+  {
+    name: "Vivian O'Connor",
+    role: "Computer Science",
+    image: VivianPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Camille Murnane",
+    role: "Cybersecurity",
+    image: CamPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Alex Guzman",
+    role: "Computer Science",
+    image: AlexPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Drew Hammer",
+    role: "Computer Science",
+    image: DrewPic,
+    linkedin: "https://www.linkedin.com/in/drew-hammer/",
+    email: "#",
+  },
+  {
+    name: "Alec Creasy",
+    role: "Computer Science",
+    image: CatPic,
+    linkedin: "#",
+    email: "#",
+  },
+  {
+    name: "Tristen Onate",
+    role: "Computer Science",
+    image: CatPic,
+    linkedin: "#",
+    email: "onatefrijol@gmail.com",
+  },
+  {
+    name: "Jack Quinn",
+    role: "Computer Science",
+    image: CatPic,
+    linkedin: "#",
+    email: "jackquinn423@gmail.com",
+  },
+];
